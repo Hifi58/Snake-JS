802d74124f60037ff6c1a3cc732958bcde092b1d
Hifi58 <gillet.tony@gmail.com>
2021-07-09 14:02:01 +0200

    Plateau de jeu et snake sans mouvements

--- game.js
+++ game.js
@@ -1,82 +1,57 @@
-function loadGame(){
+let canvas = document.getElementById('zone');
+let context = canvas.getContext('2d');
 
-    //////////////////////
-    // variables static //
-    //////////////////////
+let largeur=hauteur=20;
+let x= Math.trunc(canvas.width/2);
+let y= Math.trunc(canvas.height/2);
 
-    let DIM_X = 100;
-    let DIM_Y = 100;
+let depX=depY=0; /// Variable pour le déplacement au clavier ///
 
-    let DIR_NONE = 100;
+/////////////////////////////
+///// Paramètres du jeu /////
+/////////////////////////////
 
-    ////////////////////////////
-    // Code ASCII des flèches //
-    ////////////////////////////
+window.onload=function(){
+let intervalID = setInterval(game,10); /// Game executé toutes les  10ms ///
 
-    let Key_UP = 38;     
-    let Key_RIGHT = 39;
-    let Key_BOTTOM = 40;
-    let Key_LEFT = 37;
+    document.addEventListener("keydow",keyboard);
+}
+function game(){
+    x+=depX;
+    y+=depY;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle ="#f1c40f";
+    context.fillRect(x, y, largeur, hauteur);
+}
 
-    /////////////////////////
-    // Variable d'atributs //
-    /////////////////////////
+function keyboard(event){
+    switch(event.keyCode) {
+        case 37:
+        // touche gauche
+        depX=-1;
+        depY=0;
+        break;
+        case 38:
+        // touche haut
+        depX=0;
+        depY=-1;
+        break;
+        case 39:
+        // touche droite
+        depX=1;
+        depY=0;
+        break;
+        case 40:
+        // touche bas
+        depX=0;
+        depY=1;
+        break;
+        case 32:
+        // touche espace
+        depX=0;
+        depY=0;
+        break;
+        }
+}
 
-    let player = {};
 
-    function checkForEnding(){
-    // Optional : Check if :
-    // - player head is crashed on his own body,
-    // - isMoveOk return false
-    };
-
-    function listenToEvent(e){};
-
-    function isMoveOk(player,oneDirection){};
-
-    function onTick(){};
-
-    function checkForFruit(){};
-
-    function updatePlayerPosition(){};
-
-    ////////////////////////////////
-    // Fonction de plateau de jeu //
-    ////////////////////////////////
-
-    function createBoard(){
-        let board = document.getElementById('board');
-        board.innerHTML=" ";
-
-        let div;
-        for(let i=0; i<1; i++){
-            div = document.createElement("div");
-            div.setAttribute("class", "case");
-            board.appendChild(div);         
-        };
-
-        let fruit;
-        for(let i=0; i<90; i++){
-            fruit = document.createElement("div");
-            fruit.setAttribute("class", "fruit");
-            board.appendChild(fruit);
-        };
-    };
-
-    
-    function initGame(){
-        console.log("Chargement du jeu");
-        createBoard();
-        updatePlayerPosition();
-        // Ici: associer la fonction listenToEvent à un événement
-        //      "touche (flèche) enfoncée"
-        // Ici: déclencher une fonction onTick à intervalles 
-        //      réguliers (ex: 0.1 seconde)
-    };
-
-    initGame();
-}; // Fin de loadGame
-
-// Ici: associer la fonction loadGame à 
-//      la fin du chargement de la page HTML
-document.addEventListener("DOMContentLoaded", loadGame);
